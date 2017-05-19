101922627f7b584b40bd3e9ca0c7ec8f67f5462d
melanie.garnier <melanie.mc.garnier@gmail.com>
2017-05-19 17:10:23 +0200

    bug fixes

--- web/js/pdbSubmit.js
+++ web/js/pdbSubmit.js
@@ -256,7 +256,7 @@ pdbSubmit.prototype.nglEditionBelt = function(detList, deterAndVolumeList) {
     var belt_radius=Math.sqrt( volume / (pi * (this.halfH * 2)) + Math.pow(protein_radius,2) );
     $(this.volumeValueElem).html( sprintf("%2.1f", volume) + ' &#8491<sup>3</sup>' );
     $(this.crownValueElem).html( sprintf("%2.1f", belt_radius) + ' &#8491');
-    this.nglCorona(belt_radius, jsonFile, detList);
+    this.nglCorona(belt_radius);
 };
 
 
