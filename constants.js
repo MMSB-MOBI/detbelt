dadf23a064e56afe0c5667d3956dc29f824d45fd
cecilpert <cecile.hilpert@gmail.com>
2020-12-09 15:01:40 +0100

    dev constants

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 const SERVER_PORT = "3001"
-const DETERGENT_DB = "http://192.168.117.151:3709/" //deterDB
-const PROTEIN_DB = "http://192.168.117.151:1134/" //whiteDB
-const SERVER_URL = "https://detbelt.ibcp.fr" 
+const DETERGENT_DB = "http://localhost:3709/" //deterDB
+const PROTEIN_DB = "http://localhost:1134/" //whiteDB
+const SERVER_URL = "https://detbelt-dev.ibcp.fr" 
 
 module.exports = {
     SERVER_PORT : SERVER_PORT,
