342d364862cb43d347c4523a91c3e3e268cc1d1d
Cecile Hilpert <cecile.hilpert@gmail.com>
2020-12-09 12:39:30 +0100

    constants url and db in constants.js

---
+++ constants.js
@@ -0,0 +1,11 @@
+const SERVER_PORT = "3001"
+const DETERGENT_DB = "http://192.168.117.151:3709/" //deterDB
+const PROTEIN_DB = "http://192.168.117.151:1134/" //whiteDB
+const SERVER_URL = "https://detbelt.ibcp.fr" 
+
+module.exports = {
+    SERVER_PORT : SERVER_PORT,
+    DETERGENT_DB : DETERGENT_DB,
+    PROTEIN_DB : PROTEIN_DB,
+    SERVER_URL : SERVER_URL,
+};
