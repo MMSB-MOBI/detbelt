a24f10597ef4bb70ff25f6fb99542dd996dd718e
glaunay <pitooon@gmail.com>
2021-02-15 16:01:09 +0100

    hollow cylinder as mask

--- src/app/pdbSubmit.js
+++ src/app/pdbSubmit.js
@@ -12,6 +12,7 @@ var pdbSubmit = function(opt) {
     Core.call(this, nArgs);
     this.hollowShape = null;
     this.hollowComp  = null;
+    this.masked = true;
 }
 pdbSubmit.prototype = Object.create(Core.prototype);
 pdbSubmit.prototype.constructor = pdbSubmit;
@@ -32,16 +33,20 @@ pdbSubmit.prototype.drawControlBox = function () {
     var self = this;
     var turnBoxTag = 'turnBox_' + self.idNum;
     var extrudeTag = 'extrudeBox_' + self.idNum;
+  
 
     $(this.getNode()).find(".pdbSubmitDiv").append('<div class="ctrlBox"></div>');
     var ctrlDiv = $(this.getNode()).find(".pdbSubmitDiv .ctrlBox")[0];
     $(ctrlDiv).append('<div class="checkbox"><label><input type="checkbox" id="' + turnBoxTag + '" >Set molecule spin on/off</label></div>');
-    $(ctrlDiv).append('<div class="checkbox"><label><input type="checkbox" checked=true id="' + extrudeTag + '" >Extrude protein volume</label></div>');
+    $(ctrlDiv).append('<div class="checkbox"><label><input type="checkbox" checked=' + self.masked + ' id="' + extrudeTag + '" >Mask protein volume</label></div>');
     $( "#" + turnBoxTag).on('change',function(){
         if(document.getElementById(turnBoxTag).checked){self.stage.setSpin( [ 0, 1, 0 ], 0.01 )}
             else{self.stage.setSpin( [ 0, 1, 0 ], 0 )}
     });
-    $( "#" + extrudeTag).on('change', () => self.toggleHollow());
+    $( "#" + extrudeTag).on('change', (e) => {
+        self.masked = e.target.checked;
+        self.toggleHollow();
+    });
 };
 
 pdbSubmit.prototype.drawResultBox = function () {
@@ -91,7 +96,6 @@ pdbSubmit.prototype.setWait = function(status){
 };
 
 pdbSubmit.prototype.display = function(jsonData) {
-    console.log("pdbSubmit display", jsonData)
     //create the DOM
     //send jsonFile in argument to stock his color in object dataDetergentFromJson
     var self = this;
@@ -108,18 +112,6 @@ pdbSubmit.prototype.display = function(jsonData) {
             + '</div>'
             + '</div>');
     this.emiter.emit('display'); //event for give at submitBox compenent the size col-xs-12
-
-
-
-    /*$.getJSON(jsonFile, function (jsonData) {
-        self.dataDetergentFromJson = [];
-        for (var category in jsonData.data) {
-            jsonData.data[category].forEach(function(d){
-                 self.dataDetergentFromJson.push(d);
-            });
-        }
-
-    });*/
 
     self.dataDetergentFromJson = [];
     for (var category in jsonData.data) {
@@ -139,8 +131,6 @@ pdbSubmit.prototype.display = function(jsonData) {
         fileInput.addEventListener('change', function() {
 
             var reader = new FileReader();
-            //var re = /^REMARK +1\/2 of bilayer thickness/;
-
             reader.addEventListener('load', function() {
                 var taille=fileInput.files[0].size;
 
@@ -149,22 +139,10 @@ pdbSubmit.prototype.display = function(jsonData) {
                     alert("empty file");
                     reader.abort();
                 }
-                /*else if (! re.test(reader.result)) {
-                    alert("Corona need a pdb file");
-                    console.log("le fichier pdb doit commencer par une ligne REMARK 1/2 of bilayer thickness")
-                }*/
-                else{
-                    //console.log("azer")
+                else {
                     self.fileContent=reader.result;
-                    //console.log(typeof(self.fileContent));
                     let to_show = {"fileObject": fileInput.files[0]}
-                    //console.log(typeof(fileInput.files[0]))
                     self.showProt(to_show);
-                    //console.log($("#"+inputTag))
-                    //$("#"+inputTag).remove();           // in a function
-                    //console.log($("#"+self.divTag+" h1"))
-                    //$("#"+self.divTag+" h1").remove();  // is it usefull ? I can't find where it is in the DOM
-
                 }
             });
 
@@ -196,30 +174,24 @@ pdbSubmit.prototype.nglStart = function() {
 
     //Cecile 15/12/20 not fileObject anymore, just read from self.fileContent string. 
     
-    console.log(self.dataDetergentFromJson)
     const stringBlob = new Blob( [ self.fileContent ], { type: 'text/plain'} );
 
-    console.log("Trying to read PDB");
-    console.log(self.fileContent);
-   
     this.stage.loadFile(stringBlob, { defaultRepresentation: true, ext: "pdb" })
         .then(function (o) { // Ajouter des elements, modifier le canvas avant affichage.
             o.setDefaultAssembly('');
             o.autoView();
             self.nglStructureView_noBelt = o;
             if(self.nglStructureView_noBelt.structure.atomCount===0){
-                console.log("This file is not a pdb file");
+                console.warn("This file is not a pdb file");
+                return Promise.reject("This file is not a pdb file");
             }
             else{
                 self.emiter.emit('ngl_ok',self.fileContent);
-                //console.log("ngl_ok")
-                console.log(o)
                 $('h3').remove();                                               
                 $(self.getNode()).find(".ngl_canva").addClass("display");
                 self.stage.handleResize();
             }
         }).catch( (err) => {
-            console.log("OUPS");
             console.warn(err);
             var blockerWidget = blocker.new({root : "#main", type : "error"});
             blockerWidget.on('close', () => {                
@@ -249,11 +221,6 @@ pdbSubmit.prototype.nglRefresh = function(pdbText, data, detList) {
             o.autoView();
             self.halfH = parseFloat(self.data.halfH);
             var basis = new NGL.Matrix4();
-            /* not functional yet : we tried to make a rotation of the view (z axis switched with y axis)
-            basis.set( 1, 0, 0, 0,
-                       0, 1, 0, 0,
-                       0, 0, 1, 0,
-                       0, 0, 0, 1 );*/
             self.stage.viewerControls.align(basis);
             self.nglStructureView_belt = o;
             self.beltRadius = parseFloat(self.data.beltRadius);
@@ -278,6 +245,9 @@ pdbSubmit.prototype.nglCorona = function() {
             }
         });
     });
+    if (this.masked)
+        this.createHollow();
+    
 }
 
 pdbSubmit.prototype.createCylinder = function(volumeTOT,volumeDet,color) {
@@ -290,90 +260,32 @@ pdbSubmit.prototype.createCylinder = function(volumeTOT,volumeDet,color) {
     console.log("Color", color);
     this.oldPoint = pointY;
     this.nglStructureView_belt.autoView();
-    this.toggleHollow();
+   
 }
 
-/* GL TO DO
- * Add button/ set as default/ recompute on corona refresh
- * hook destructor calls
- *
- * 
- * */
-pdbSubmit.prototype.toggleHollow = function() {
+pdbSubmit.prototype.toggleHollow = function() {
     if(this.hollowShape != null) {
-        console.log("hiding hollow")
         this.hollowComp.removeAllRepresentations();        
         this.stage.removeComponent(this.hollowShape);
         this.hollowComp = null;
-        this.hollowShape = null; // Memory leak need to dispose materials/mesh
+        this.hollowShape = null; // Could be Memory leak need to dispose materials/mesh
         return;
     };
+    this.createHollow();
+}
+
+pdbSubmit.prototype.createHollow = function() {
+   
     const hollowHeight = this.halfH + 0.25;
 	const hollowHeightOffset = hollowHeight * (-1);
 	const hollowRadius = this.proteinRadius;
-    //const color5 = new THREE.Color( 'skyblue' );
-    console.log("showing hollow")
-    console.log(`v1 ${[0, hollowHeightOffset, 0]},${[0, hollowHeight, 0]}`); 
-
-    console.log(`proteinRadius ${this.proteinRadius}, belt Radius ${this.beltRadius}`);
 	this.hollowShape = new NGL.Shape("hollowShape", { disableImpostor: true } );
-	
-
-  
-	console.log([169,169,169], hollowRadius);
-
-	
     this.hollowShape.addCylinder( [0, hollowHeightOffset, 0], 
 				 [0, hollowHeight      , 0], 
-				 /*[0,0,0]*/[245,245,245], hollowRadius);
-    /*
-   this.hollowShape.addCylinder( [0, this.oldPoint, 0], [0, this.pointY, 0], 
-                                 [0, 0, 0]            , hollowRadius);
-*/
+				 [0,0,0]/*[245,245,245]*/, hollowRadius);
 	this.hollowComp = this.stage.addComponentFromObject(this.hollowShape) 
 	this.hollowComp.addRepresentation( "hollow", { "opacity" : 0.6 } );
 }
-
-pdbSubmit.prototype.toggleExtrude = function() {
-    if(this.hollowShape != null) {
-        console.log("hiding extrude")
-        this.hollowComp.removeAllRepresentations();        
-        this.stage.removeComponent(this.hollowShape);
-        this.hollowComp = null;
-        this.hollowShape = null; // Memory leak need to dispose materials/mesh
-        return;
-    };
-    const hollowHeight = this.halfH + 0.25;
-	const hollowHeightOffset = hollowHeight * (-1);
-	const hollowRadius = this.proteinRadius;
-    //const color5 = new THREE.Color( 'skyblue' );
-    console.log("showing hollow")
-    console.log(`v1 ${[0, hollowHeightOffset, 0]},${[0, hollowHeight, 0]}`); 
-
-    console.log(`proteinRadius ${this.proteinRadius}, belt Radius ${this.beltRadius}`);
-	this.hollowShape = new NGL.Shape("hollowShape", { disableImpostor: true } );
-	
-
-  
-	console.log([169,169,169], hollowRadius);
-
-	
-    this.hollowShape.addCylinder( [0, hollowHeightOffset, 0], 
-				 [0, hollowHeight      , 0], 
-				 [0,0,0], hollowRadius);
-    /*
-   this.hollowShape.addCylinder( [0, this.oldPoint, 0], [0, this.pointY, 0], 
-                                 [0, 0, 0]            , hollowRadius);
-*/
-	this.hollowComp = this.stage.addComponentFromObject(this.hollowShape) 
-	this.hollowComp.addRepresentation( "hollow", { "opacity" : 0.5 } );
-}
-var extrudeSettings = {
-    amount : 2,
-    steps : 1,
-    bevelEnabled: false,
-    curveSegments: 8
-};
 
 pdbSubmit.prototype.nglEditionData = function(detList) {
     //fct to calculate the new volume and edit the crown after the change of the detergents stored in detList
@@ -382,7 +294,6 @@ pdbSubmit.prototype.nglEditionData = function(detList) {
     this.volumeTOT = 0;
     var pi = 3.1415;
     this.proteinRadius = parseFloat(this.data.proteinRadius);
-    console.log("dataDetergentFromJson", self.dataDetergentFromJson); 
     this.detList.forEach(function(e){
         var name = e.detName;
         self.dataDetergentFromJson.forEach(function(i){
@@ -395,9 +306,7 @@ pdbSubmit.prototype.nglEditionData = function(detList) {
             }
         });
     });
-    console.log("nglEditionData volumeTot", self.volumeTOT)
     this.beltRadius=Math.sqrt( this.volumeTOT / (pi * (this.halfH * 2)) + Math.pow(this.proteinRadius,2) );
-    console.log("beltRadius", this.beltRadius)
     $(this.volumeValueElem).html( sprintf("%2.1f", this.volumeTOT) + ' &#8491<sup>3</sup>' );
     $(this.crownValueElem).html( sprintf("%2.1f", this.beltRadius) + ' &#8491');
     this.nglCorona();
@@ -405,8 +314,6 @@ pdbSubmit.prototype.nglEditionData = function(detList) {
 
 pdbSubmit.prototype.removeOldCorona = function(detList) {
     self = this;
-    console.log("NGLCorona compList : ");
-    console.log(this.stage.compList);
     var iToDel = [];
     this.stage.compList.forEach(function(e, i){
         if(i === 0) return;
@@ -416,12 +323,10 @@ pdbSubmit.prototype.removeOldCorona = function(detList) {
     iToDel.forEach(function(e){
         self.stage.removeComponent(self.stage.compList[e]);
     });
-    console.log(this.stage.compList);
     this.nglEditionData(detList);
 }
 
 pdbSubmit.prototype.getCoronaData = function() {
-    console.log("getCoronaData");
     return {
             "halfH" : this.halfH,
             "proteinRadius" : this.proteinRadius,
